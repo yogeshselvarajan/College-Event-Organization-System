a31874ac46ec6677e33d8447d168e9823c07a095
yogeshcenation <yogeshselvarajan@gmail.com>
2021-11-20 18:02:05 +0530

    Updated Code

---
+++ project/college/event/organizer/LoginPage.java
@@ -0,0 +1,81 @@
+package project.college.event.organizer;
+
+import java.awt.BorderLayout;
+import java.awt.GridLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.WindowAdapter;
+import java.awt.event.WindowEvent;
+
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+import javax.swing.JPasswordField;
+import javax.swing.JTextField;
+
+class LoginDemo extends JFrame implements ActionListener {
+
+    JPanel panel;
+    JLabel user_label, password_label, message;
+    JTextField userName_text;
+    JPasswordField password_text;
+    JButton submit, cancel;
+
+    LoginDemo() {
+
+        // User Label
+        user_label = new JLabel();
+        user_label.setText("User Name :");
+        userName_text = new JTextField();
+
+        // Password
+
+        password_label = new JLabel();
+        password_label.setText("Password :");
+        password_text = new JPasswordField();
+
+        // Submit
+
+        submit = new JButton("SUBMIT");
+
+        panel = new JPanel(new GridLayout(3, 1));
+
+        panel.add(user_label);
+        panel.add(userName_text);
+        panel.add(password_label);
+        panel.add(password_text);
+
+        message = new JLabel();
+        panel.add(message);
+        panel.add(submit);
+
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+        // Adding the listeners to components..
+        submit.addActionListener(this);
+        add(panel, BorderLayout.CENTER);
+        setTitle("Please Login Here !");
+        setSize(300, 100);
+        setVisible(true);
+
+    }
+
+    public static void main(String[] args) {
+        new LoginDemo();
+    }
+
+    @Override
+    public void actionPerformed(ActionEvent ae) {
+        String userName = userName_text.getText();
+        String password = password_text.getText();
+        if (userName.trim().equals("admin") && password.trim().equals("admin")) {
+            message.setText(" Hello " + userName
+                    + "");
+        } else {
+            message.setText(" Invalid user.. ");
+        }
+
+    }
+
+}
