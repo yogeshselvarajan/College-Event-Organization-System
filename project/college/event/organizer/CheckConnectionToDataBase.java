1ccfa2651e3890007442acb2274cb9b80fcebe9d
yogeshcenation <yogeshselvarajan@gmail.com>
2021-11-28 11:50:59 +0530

    This will check whether the DB can be connected or not

---
+++ project/college/event/organizer/CheckConnectionToDataBase.java
@@ -0,0 +1,18 @@
+package project.college.event.organizer;
+
+import java.sql.*;
+
+import java.sql.Statement;
+
+public class CheckConnectionToDataBase {
+
+        public static void main(String[] args) {
+            try {
+                Connection connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/projectdb", "root", "localhost");//Establishing connection
+                System.out.println("Connected With the database successfully");
+            } catch (SQLException e) {
+                System.out.println("Error while connecting to the database");
+            }
+        }
+
+    }
