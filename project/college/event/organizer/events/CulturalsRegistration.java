7ee0e86f20370a164690a0267958c3979d9dcf3a
yogeshcenation <yogeshselvarajan@gmail.com>
2021-12-01 20:38:59 +0530

    Updated Code for CulturalsRegistration.java

---
+++ project/college/event/organizer/events/CulturalsRegistration.java
@@ -0,0 +1,194 @@
+package project.college.event.organizer.events;
+
+import project.college.event.organizer.login.RedirectToLogin;
+
+import javax.swing.*;
+import java.awt.*;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.Statement;
+
+public class CulturalsRegistration
+{
+    public CulturalsRegistration()
+    {
+        // Creating the Frame and then setting the Title
+        JFrame f =new JFrame("SCHEDULE AN EVENT");
+        JLabel title = new JLabel("CULTURALS FEST REGISTRATION FORM",JLabel.CENTER);
+        title.setBounds(400,05,600,25);
+        title.setFont(new Font("Comic Sans MS", Font.BOLD, 20));
+        f.add(title);
+        f.setBackground(Color.cyan);
+        f.setForeground(Color.black);
+
+        /** Declaring the initial variables of the type JLabel for all the necessary labels */
+        JLabel student_name,year,department,age,college_name;
+        JLabel contact_num,name_of_faculty,email_id;
+        JLabel events_available;
+
+        /** "Student Name" Label and TextField Definition */
+        student_name = new JLabel("Student Name ",JLabel.LEFT);
+        student_name.setBounds(250,50,200,25);
+        student_name.setFont(new Font("Calibri", Font.ITALIC, 18));
+        JTextField student_name_field = new JTextField();
+        student_name_field.setBounds(400,45,250,25);
+        f.add(student_name);f.add(student_name_field);
+
+        /** "Department" Label and ChoiceBox Definition */
+        department = new JLabel(" Department ",JLabel.LEFT);
+        department.setBounds(250,90,120,25);
+        department.setFont(new Font("Calibri", Font.ITALIC, 18));
+        f.add(department);
+        String[] question_course_stream =
+                {
+                        "Select your Course",
+                        "B.E. Computer Science and Engineering",
+                        "B.E. Artificial Intelligence and Data Science",
+                        "B.E. BioTechnology",
+                        "B.E. BioMedical Engineering",
+                        "B.E.Electronics and Communication Engineering",
+                        "B.E. Information Technology",
+                        "B.E. Mechanical Engineering",
+                        "B.E. Civil Engineering",
+                };
+        JComboBox department_box = new JComboBox(question_course_stream);
+        department_box.setBounds(400,90,300,30);
+        f.add(department_box);
+
+        /** "Year and Section" Label and TextField Definition */
+        year = new JLabel("Year & Section ");
+        year.setBounds(250,130,120,25);
+        year.setFont(new Font("Calibri", Font.ITALIC, 18));
+        f.add(year);
+        JTextField year_field = new JTextField();
+        year_field.setBounds(400,130,250,25);
+        f.add(year_field);
+
+        /** "Age" Label and TextField Definition */
+        age = new JLabel("Age ");
+        age.setBounds(250,170,120,25);
+        age.setFont(new Font("Calibri", Font.ITALIC, 18));
+        f.add(age);
+        JTextField age_field = new JTextField();
+        age_field.setBounds(400,170,50,25);
+        f.add(age_field);
+
+        /** "College Name" Label and TextField Definition */
+        college_name = new JLabel("College Name ");
+        college_name.setBounds(250,210,120,25);
+        college_name.setFont(new Font("Calibri", Font.ITALIC, 18));
+        f.add(college_name);
+        JTextField college_name_field = new JTextField();
+        college_name_field.setBounds(400,210,250,25);
+        f.add(college_name_field);
+
+        /** "Contact Number" Label and TextField Definition */
+        contact_num = new JLabel("Contact Number ");
+        contact_num.setBounds(250,250,150,25);
+        contact_num.setFont(new Font("Calibri", Font.ITALIC, 17));
+        f.add(contact_num);
+        JTextField contact_num_field = new JTextField();
+        contact_num_field.setBounds(400,250,250,25);
+        f.add(contact_num_field);
+
+        /** "Name of Faculty" Label and TextField Definition */
+        name_of_faculty = new JLabel("Name of Faculty ");
+        name_of_faculty.setBounds(250,290,150,25);
+        name_of_faculty.setFont(new Font("Calibri", Font.ITALIC, 17));
+        f.add(name_of_faculty);
+        JTextField name_of_faculty_field = new JTextField();
+        name_of_faculty_field.setBounds(400,290,250,25);
+        f.add(name_of_faculty_field);
+
+        /** "Email ID" Label and TextField Definition */
+        email_id = new JLabel("Email ID ");
+        email_id.setBounds(250,330,150,25);
+        email_id.setFont(new Font("Calibri", Font.ITALIC, 17));
+        f.add(email_id);
+        JTextField email_id_field = new JTextField();
+        email_id_field.setBounds(400,330,250,25);
+        f.add(email_id_field);
+
+        /** "Events Available" Label and ChoiceBox Definition */
+        events_available = new JLabel("Events Available");
+        events_available.setBounds(250,370,150,25);
+        events_available.setFont(new Font("Calibri", Font.ITALIC, 17));
+        f.add(events_available);
+        CheckboxGroup cbg=new CheckboxGroup();
+        Checkbox ck1=new Checkbox("Solo Singing",false,cbg);
+        ck1.setBounds(400,370,200,30);
+        Checkbox ck2=new Checkbox("Jam",false,cbg);
+        ck2.setBounds(400,395,80,30);
+        Checkbox ck3 = new Checkbox("Classical Bhajan",false,cbg);
+        ck3.setBounds(400,420,200,30);
+        Checkbox ck4 = new Checkbox("Rock Band",false,cbg);
+        ck4.setBounds(400,455,200,30);
+        Checkbox ck5 = new Checkbox("Debate",false,cbg);
+        ck5.setBounds(400,480,200,30);
+        Checkbox ck6 = new Checkbox("Group Dance",false,cbg);
+        ck6.setBounds(400,505,200,30);
+        Checkbox ck7 = new Checkbox("Bharathanatyam",false,cbg);
+        ck7.setBounds(400,530,200,30);
+        Checkbox ck8 = new Checkbox("Instrumental Fashion",false,cbg);
+        ck8.setBounds(400,555,200,30);
+        Checkbox ck9 = new Checkbox("Brass Band",false,cbg);
+        ck9.setBounds(400,580,200,30);
+        Checkbox ck10 = new Checkbox("Battle of Bands",false,cbg);
+        ck10.setBounds(400,605,200,30);
+        Checkbox ck11 = new Checkbox("Folk arts and Music",false,cbg);
+        ck11.setBounds(400,630,200,30);
+        Checkbox ck12 = new Checkbox("Quiz",false,cbg);
+        ck12.setBounds(400,655,200,30);
+        f.add(ck1);f.add(ck2);f.add(ck3);f.add(ck4);f.add(ck5);
+        f.add(ck6);f.add(ck7);f.add(ck8);f.add(ck9);f.add(ck10);
+        f.add(ck11);f.add(ck12);
+
+        JButton submit = new JButton("Submit");
+        submit.setBounds(500,690,100,30);
+        f.add(submit);
+
+        submit.addActionListener( e ->
+        {
+            String Name = student_name_field.getText();
+            String Department = department_box.getSelectedItem().toString();
+            String Year_Section = year_field.getText();
+            String Age = age_field.getText();
+            String College_Name = college_name_field.getText();
+            String Contact_Number = contact_num_field.getText();
+            String Name_of_Faculty = name_of_faculty_field.getText();
+            String Email = email_id_field.getText();
+            String Event_Choosen = cbg.getSelectedCheckbox().getLabel();
+
+            try {
+                Connection connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/projectdb", "root", "localhost");
+
+                String query = "INSERT INTO culturals values('" + Name + "','" + Department + "','" + Year_Section + "','" +
+                        Age + "','" + College_Name + "','" + Contact_Number + "','" + Name_of_Faculty + "','" + Email + "','" + Event_Choosen + "')";
+
+                Statement sta = connection.createStatement();
+                int x = sta.executeUpdate(query);
+                if (x == 0) {
+                    JOptionPane.showMessageDialog(submit, "An Error Occurred ! Retry again !");
+                } else {
+                    JOptionPane.showMessageDialog(submit,
+                            "You have been successfully registered for the Culturals.  ");
+                }
+                connection.close();
+            } catch (Exception exception) {
+                exception.printStackTrace();
+            }
+        });
+
+        /**  Frame Properties Declarations  */
+        f.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        f.setSize(1600,775);
+        f.setLayout(null);
+        f.setVisible(true);
+    }
+    public  static void main(String argv[])
+    {
+        new CulturalsRegistration();
+    }
+
+    }
+
